2cd4a7c5a64b63f9002882ab1aec03fd2470207a
Calle Pettersson <calle@cape.nu>
2019-04-27 12:32:06 +0200

    Add support for lists

--- sops/data_sops_file.go
+++ sops/data_sops_file.go
@@ -98,23 +98,50 @@ func dataSourceFileRead(d *schema.ResourceData, meta interface{}) error {
 //
 // All keys will be joined by dot
 // e.g. {"a": {"b":"c"}} => {"a.b":"c"}
+// or {"a": {"b":[1,2]}} => {"a.b.0":1, "a.b.1": 2}
 func flatten(data map[string]interface{}) map[string]string {
-	flattened := map[string]string{}
-	for key, value := range data {
-		switch t := value.(type) {
-		case string, int:
-			flattened[key] = fmt.Sprint(t)
+	ret := make(map[string]string)
+	for k, v := range data {
+		switch typed := v.(type) {
 		case map[interface{}]interface{}:
-			f := flatten(convertMap(t))
-			for k, v := range f {
-				// Join all keys with dot
-				flattened[fmt.Sprintf("%s.%s", key, k)] = v
+			for fk, fv := range flatten(convertMap(typed)) {
+				ret[fmt.Sprintf("%s.%s", k, fk)] = fv
+			}
+		case map[string]interface{}:
+			for fk, fv := range flatten(typed) {
+				ret[fmt.Sprintf("%s.%s", k, fk)] = fv
+			}
+		case []interface{}:
+			for fk, fv := range flattenSlice(typed) {
+				ret[fmt.Sprintf("%s.%s", k, fk)] = fv
 			}
 		default:
-			fmt.Printf("unexpected type %T", t)
+			ret[k] = fmt.Sprint(typed)
 		}
 	}
-	return flattened
+	return ret
+}
+func flattenSlice(data []interface{}) map[string]string {
+	ret := make(map[string]string)
+	for idx, v := range data {
+		switch typed := v.(type) {
+		case map[interface{}]interface{}:
+			for fk, fv := range flatten(convertMap(typed)) {
+				ret[fmt.Sprintf("%d.%s", idx, fk)] = fv
+			}
+		case map[string]interface{}:
+			for fk, fv := range flatten(typed) {
+				ret[fmt.Sprintf("%d.%s", idx, fk)] = fv
+			}
+		case []interface{}:
+			for fk, fv := range flattenSlice(typed) {
+				ret[fmt.Sprintf("%d.%s", idx, fk)] = fv
+			}
+		default:
+			ret[fmt.Sprint(idx)] = fmt.Sprint(typed)
+		}
+	}
+	return ret
 }
 
 func convertMap(originalMap map[interface{}]interface{}) map[string]interface{} {
