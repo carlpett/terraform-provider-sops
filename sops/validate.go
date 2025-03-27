89c2053af39e064919076ab08ed66d1da98f3c71
Calle Pettersson <calle@cape.nu>
2025-03-27 20:46:22 +0100

    Simplify file type validation

--- sops/validate.go
+++ sops/validate.go
@@ -2,20 +2,18 @@ package sops
 
 import "fmt"
 
+var validTypes = map[string]bool{
+	"json":   true,
+	"yaml": true,
+	"dotenv": true,
+	"ini": true,
+	"raw": true,
+}
+
 // validateInputType ensures that we can decode the input
 func validateInputType(inputType string) error {
-	switch inputType {
-	case "json":
+	if _, ok := validTypes[inputType]; ok {
 		return nil
-	case "yaml":
-		return nil
-	case "dotenv":
-		return nil
-	case "ini":
-		return nil
-	case "raw":
-		return nil
-	default:
-		return fmt.Errorf("Don't know how to decode file with input type %s, set input_type to json, yaml, ini, dotenv or raw as appropriate", inputType)
 	}
+	return fmt.Errorf("Don't know how to decode file with input type %s, set input_type as appropriate", inputType)
 }
